affb883ecaf8ab36642d5c423de12956a9e21f6c
Rohit Nath <rohitnath49195@gmail.com>
2025-10-03 10:10:38 +0530

    Update NameMeter.py

--- NameMeter.py
+++ NameMeter.py
@@ -91,7 +91,7 @@ Vibez of {r['name']} 🎭
 💪 Confidence: {r['confidence']}%  
 🐍 Jealousy: {r['jealousy']}%  
 
-👉 Check yours here: https://namemeterpy.streamlit.app/  
+👉 Check yours here: https://namemeter.streamlit.app/  
 """  
     whatsapp_url = "https://api.whatsapp.com/send?text=" + urllib.parse.quote(share_text)  
     insta_url = "https://www.instagram.com/?url=" + urllib.parse.quote("https://NameMeter.com")  
